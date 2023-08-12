d2c108dfcb7741cede951a7fc6fd9edff2a6916d
Ijoelel <fianarnatha13@gmail.com>
2023-08-13 00:29:21 +0700

    update kode

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const kupon = [
-    3451, 2345, 1234, 5242, 1231, 4564, 23563, 2346, 7956, 3673, 5673, 2387,
+	001052, 001051, 001050, 001049, 001048, 001196, 001197, 001198, 001199,
+	001200, 001179, 001178, 001177, 001176, 001175, 001081, 001082, 001083,
+	001084, 001085, 001120, 001121, 001122, 001123, 001124, 001125, 001126,
+	001127, 001128, 001129,
 ];
 const input = document.querySelector("input");
-const wrapper = document.querySelector('.wrapper')
-const message = document.createElement("h2")
+const wrapper = document.querySelector(".wrapper");
+const message = document.createElement("h2");
 
 const findHandler = () => {
-    const enteredValue = input.valueAsNumber;
-    const existsInKupon = kupon.includes(enteredValue);
-    if (existsInKupon) {
-        message.innerHTML = 'Kode Ditemukan'
-    } else {
-        message.innerHTML = 'Kode Tidak Ditemukan'
-    }
-    
-    wrapper.appendChild(message)
+	const enteredValue = input.valueAsNumber;
+	const existsInKupon = kupon.includes(enteredValue);
+	if (existsInKupon) {
+		message.innerHTML = "Kode Ditemukan";
+        console.log(kupon.length)
+	} else {
+		message.innerHTML = "Kode Tidak Ditemukan";
+	}
+
+	wrapper.appendChild(message);
 };
-
-
-
